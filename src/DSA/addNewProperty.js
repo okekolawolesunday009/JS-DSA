ecdd3f75e83d259cf7d20dfbbaf55954992fae5a
OkeKolawoleSunday009 <OkeKolawoleSunday@gmail.com>
2024-07-31 03:20:41 +0100

    update READEME, added new DSA task

--- src/DSA/addNewProperty.js
+++ src/DSA/addNewProperty.js
@@ -4,4 +4,18 @@
 //where propName is the key and propValue is the 
 //corresponding value. The function should return a boolean indicating that the property exists.
 
- 
+function addNewProperty(obj, propName, propValue) {
+	// Write your code below
+	if (typeof obj == 'object') {
+		obj[propName] = propValue;			
+		console.log(obj);
+	}
+	return obj.hasOwnProperty(propName);
+
+  
+  
+} 
+addNewProperty(
+	{kola: 2, dare: 12},
+	"dddff", 13
+)
